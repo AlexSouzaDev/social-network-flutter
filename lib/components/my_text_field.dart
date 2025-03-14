9804a41d910a9586972c7ee075c626f24f42efd4
AlexSouza <alexsouzadev@gmail.com>
2025-03-14 09:15:09 +0000

    email and pw text field in the login page

---
+++ lib/components/my_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+// text field
+// a box where the user can enter text
+
+// widget that needs a text controller, hint text, and a label
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // login page
 // an existing user can login using email and password
+// once logged in, the user can view the home page
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,8 +12,50 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //ui elements
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    //scatffold
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+
+      //body
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+              //logo
+              Icon(
+                Icons.lock_open_rounded,
+                size: 72,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              const SizedBox(height: 50),
+
+              //message
+              Text(
+                "Welcome back, you\'ve been missed!",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 25),
+              //email
+
+              //password
+
+              //forgot password
+
+              //login button
+
+              //register now
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
